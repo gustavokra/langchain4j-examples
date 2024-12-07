8ccc568e804d63967a92f96b5b73cba131e88ab4
gustavokra <gustavo.kraemer6@gmail.com>
2024-12-07 16:28:56 -0300

    adicionando explicitação do modelo utilizado, para controle de memória

--- src/main/java/com/kraemer/MyAiChatService.java
+++ src/main/java/com/kraemer/MyAiChatService.java
@@ -3,7 +3,7 @@ package com.kraemer;
 import dev.langchain4j.service.UserMessage;
 import io.quarkiverse.langchain4j.RegisterAiService;
 
-@RegisterAiService
+@RegisterAiService(modelName="m1")
 public interface MyAiChatService {
     
     String chat(@UserMessage String userMessage);
