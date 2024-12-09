65346c2dd231f9457c18852e9548cd79e7061eab
gustavokra <gustavo.kraemer6@gmail.com>
2024-12-08 23:08:43 -0300

    tools podem ser adicionados a um método com @ToolBox

--- src/main/java/com/kraemer/MyAiChatService.java
+++ src/main/java/com/kraemer/MyAiChatService.java
@@ -6,11 +6,13 @@ import com.kraemer.repositories.CustomerRepository;
 import dev.langchain4j.service.MemoryId;
 import dev.langchain4j.service.UserMessage;
 import io.quarkiverse.langchain4j.RegisterAiService;
+import io.quarkiverse.langchain4j.ToolBox;
 
+// tools = { CustomerRepository.class },
 @RegisterAiService(modelName = "m1", 
-        tools = { CustomerRepository.class },
         chatMemoryProviderSupplier = CustomMessageWindowProvider.class)
 public interface MyAiChatService {
 
+    @ToolBox(value = { CustomerRepository.class })
     String chat(@MemoryId int memoryId, @UserMessage String userMessage);
 }
